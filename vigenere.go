6d4dc9c523249d3480e317a4a941c668d5d103dc
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-08-15 11:49:24 -0500

    got a lot of the encoding working - special chars are still a bit broken due to interator not being ignored
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- vigenere.go
+++ vigenere.go
@@ -17,7 +17,7 @@ type RingBuffer struct {
 func main() {
 	// arg vars
 
-	// TODO: Enum for this!
+	// TODO: Enum for mode!
 	var mode string
 	var input string
 	var key string
@@ -55,8 +55,7 @@ func checkcase(input rune) rune {
 		return 'A'
 	} else {
 		// not a number
-		// calling function should subtract itself so the shift is 0
-		return input
+		return 0
 	}
 }
 
@@ -64,7 +63,13 @@ func get_shiftmap(key string, mode string) []rune {
 	// create a slice containing alphabetical diff from a
 	var shiftmap []rune
 	for _, char := range key {
-		shift := char - checkcase(char)
+		base := checkcase(char)
+		if base == 0 {
+			// not a char
+			// skip iteration
+			continue
+		}
+		shift := char - base
 		if mode == "decode" {
 			// add a negative number for decoding
 			shift = shift * -1
@@ -92,11 +97,28 @@ func popbuff(rb RingBuffer, shiftmap []rune) RingBuffer {
 }
 
 func apply_shift(input string, rb RingBuffer) {
-	//(plaintext letter + shift letter - 'a') % MAXLEN
+	// create output buffer
+	var output []rune
+	var ulcase [MAXLEN]rune
 	for i, c := range input {
 		currkey := rb.shiftmap[i%len(rb.shiftmap)]
-		combo := c + currkey
-		encoded := int(combo)%MAXLEN + int(checkcase(c))
-		fmt.Printf("%c\n", rune(encoded))
+		// TODO figure out case
+		// this is a mess that rechecks case.
+		// need to find a more elegant DRY way to handle this
+		if checkcase(c) == 'a' {
+			ulcase = rb.lower
+		} else if checkcase(c) == 'A' {
+			ulcase = rb.upper
+		} else {
+			// Not a letter
+			// write the char directly and end current iteration
+			output = append(output, c)
+			continue
+		}
+		combo := ulcase[int(c+currkey-'a')%MAXLEN]
+
+		// add to output
+		output = append(output, combo)
 	}
+	fmt.Println(string(output))
 }
