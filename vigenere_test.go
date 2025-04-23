aec68125ec077669c877c1d89b4edd667c03767b
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-04-22 23:30:58 -0500

    add upper case tests
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- vigenere_test.go
+++ vigenere_test.go
@@ -17,7 +17,12 @@ func TestRingBuffPopulate(t *testing.T) {
 		}
 	})
 
-	//	t.Run("test uppercase population", func(t *testing.T) {
-	//		got_upper := rb.upper
-	//	})
+	t.Run("test uppercase population", func(t *testing.T) {
+		got_upper := rb.upper
+		want_upper := [26]rune{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+
+		if got_upper != want_upper {
+			t.Errorf("buffer was not populated with upper case letters, got %c want %c", got_upper, want_upper)
+		}
+	})
 }
